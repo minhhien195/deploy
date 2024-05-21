3f4c678e2c43b8ebf0c1f6d0baa96a6d58c640a2
minhhien195 <60498230+minhhien195@users.noreply.github.com>
2024-05-21 22:34:38 +0700

    Update playmultiController.js

--- src/app/controller/playmultiController.js
+++ src/app/controller/playmultiController.js
@@ -2,7 +2,7 @@ const songSchema = require('../models/Song');
 const Room = require('../models/Room'); 
 const User = require('../models/User');
 const UserGoogle = require('../models/UserGoogle');
-const { io, app } = require('../..');
+const { io } = require('../..');
 let roomid = "";
 let room = [];
 
